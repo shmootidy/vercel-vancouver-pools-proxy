d2759e8c94917bceafd75aa8a1859444ef9fa8dd
shmoo <shmooritchie@gmail.com>
2025-02-13 22:32:14 -0800

    set headers

--- api/getPoolsByID.js
+++ api/getPoolsByID.js
@@ -1,6 +1,25 @@
 import supabase from './supabaseClient.js'
 
 export default async function getPoolsByID(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Accept, Authorization'
+    )
+    res.setHeader('Content-Type', 'application/json')
+    return res.status(204).end() // Respond with no content for OPTIONS request
+  }
+
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Accept, Authorization'
+  )
+  res.setHeader('Content-Type', 'application/json')
+
   const { poolIDs } = req.query
 
   if (!poolIDs) {
